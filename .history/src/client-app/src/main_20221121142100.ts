5200540d91ad8d07866f49ca2f1d9269d430524d
Iliescu Mihail Dorin <31242262+Iliescu-Dorin@users.noreply.github.com>
2022-11-29 21:44:46 +0200

    Delete .history/src directory

--- .history/src/client-app/src/main_20221121142100.ts
+++
@@ -1,6 +0,0 @@
-import "@angular/compiler";
-import { bootstrapApplication } from '@angular/platform-browser';
-import "zone.js";
-import { AppComponent } from './app.component';
-
-bootstrapApplication(AppComponent)
