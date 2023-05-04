2d6183d8bbe5333a8accef725245ea42b3870098
Restyled.io <commits@restyled.io>
2023-05-04 23:00:21 +0000

    🧹 Restyled by prettier

--- src/UI/client-app/gulpfile.js
+++ src/UI/client-app/gulpfile.js
@@ -1,20 +1,23 @@
-import gulp from 'gulp'
-import gulpBrotli from 'gulp-brotli'
-import zlib from 'zlib'
+import gulp from "gulp";
+import gulpBrotli from "gulp-brotli";
+import zlib from "zlib";
 
 export function compressBrotli() {
-  return gulp.src(`./dist/client-app/**/*.{js,css,html}`)
-      .pipe(gulpBrotli({
-        extension : 'brotli',
-        skipLarger : true,
+  return gulp
+    .src(`./dist/client-app/**/*.{js,css,html}`)
+    .pipe(
+      gulpBrotli({
+        extension: "brotli",
+        skipLarger: true,
         // the options are documented at
         // https://nodejs.org/docs/latest-v10.x/api/zlib.html#zlib_class_brotlioptions
-        params : {
+        params: {
           // brotli parameters are documented at
           // https://nodejs.org/docs/latest-v10.x/api/zlib.html#zlib_brotli_constants
-          [zlib.constants.BROTLI_PARAM_QUALITY] :
-              zlib.constants.BROTLI_MAX_QUALITY,
+          [zlib.constants.BROTLI_PARAM_QUALITY]:
+            zlib.constants.BROTLI_MAX_QUALITY,
         },
-      }))
-      .pipe(gulp.dest(`./dist/compress/client-app/`))
+      })
+    )
+    .pipe(gulp.dest(`./dist/compress/client-app/`));
 }
